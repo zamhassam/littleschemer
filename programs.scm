a248de0cd5163b2e5aad0cd9e11d7f670a0a7793
zameer.hassam <zameer.hassam@transficc.com>
2019-12-30 09:51:01 +0000

    Finished chapter 3

--- programs.scm
+++ programs.scm
@@ -54,8 +54,42 @@
       ((eq? old (car lat)) (cons new (cdr lat)))
       (else (cons (car lat) (subst new old (cdr lat)))))))
 
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) (quote()))
+      ((or
+        (eq? o1 (car lat))
+        (eq? o2 (car lat)))
+       (cons new (cdr lat)))
+      (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
 
-(lat? (quote(bacon and eggs)))
-(insertR (car (quote(topping))) (car (quote(fudge))) (quote(ice cream with fudge for fudge dessert)))
-(insertL (car (quote(topping))) (car (quote(fudge))) (quote(ice cream with fudge for fudge dessert)))
-(subst (car (quote(topping))) (car (quote(fudge))) (quote(ice cream with fudge for fudge dessert)))
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote()))
+      ((eq? a (car lat)) (multirember a (cdr lat)))
+      (else (cons (car lat) (multirember a (cdr lat)))))))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote()))
+      ((eq? old (car lat)) (cons old (cons new (multiinsertR new old (cdr lat)))))
+      (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote()))
+      ((eq? old (car lat)) (cons new (cons old (multiinsertL new old (cdr lat)))))
+      (else (cons (car lat) (multiinsertL new old (cdr lat)))))))
+
+;(lat? (quote(bacon and eggs)))
+;(insertR (car (quote(topping))) (car (quote(fudge))) (quote(ice cream with fudge for fudge dessert)))
+;(insertL (car (quote(topping))) (car (quote(fudge))) (quote(ice cream with fudge for fudge dessert)))
+;(subst (car (quote(topping))) (car (quote(fudge))) (quote(ice cream with fudge for fudge dessert)))
+;(subst2 (car (quote(vanilla))) (car (quote(chocolate))) (car (quote(banana))) (quote(banana ice cream with chocolate topping)))
+;(multirember (car (quote(cup))) (quote(coffee cup tea cup and hick cup)))
+(multiinsertR (car (quote(topping))) (car (quote(fudge))) (quote(ice cream with fudge for fudge dessert)))
+(multiinsertL (car (quote(topping))) (car (quote(fudge))) (quote(ice cream with fudge for fudge dessert)))
